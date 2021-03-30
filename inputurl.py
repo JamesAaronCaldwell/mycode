53ff5591acca597a186e7868e41b295674be27c9
Aaron <james.aaron.caldwell@gmail.com>
2021-03-30 12:57:06 +0000

    learning about input statement

---
+++ inputurl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+''' Aaron Caldwell - james.aaron.caldwell@gmail.com
+best practice script'''
+#constant example
+ACONST = "www.google.com"
+
+
+def main():
+    #Input is a standard lib func
+    answer = input("what is the url you would like looked up?")
+    print(answer)
+    #finish labs up to 14 to catch up
+
+
